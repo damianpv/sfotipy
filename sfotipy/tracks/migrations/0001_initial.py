21e790c131559e819879859d39d33776bdf9dad3
Damian Perez <damian@matrix.(none)>
2014-04-12 02:03:02 -0500

    CHG: Changes

--- sfotipy/tracks/migrations/0001_initial.py
+++ sfotipy/tracks/migrations/0001_initial.py
@@ -14,6 +14,8 @@ class Migration(SchemaMigration):
             ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('order', self.gf('django.db.models.fields.PositiveIntegerField')()),
             ('track_file', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
+            ('album', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['albums.Album'])),
+            ('artist', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['artists.Artist'])),
         ))
         db.send_create_signal(u'tracks', ['Track'])
 
@@ -24,8 +26,24 @@ class Migration(SchemaMigration):
 
 
     models = {
+        u'albums.album': {
+            'Meta': {'object_name': 'Album'},
+            'artist': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['artists.Artist']"}),
+            'cover': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'artists.artist': {
+            'Meta': {'object_name': 'Artist'},
+            'biography': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
         u'tracks.track': {
             'Meta': {'object_name': 'Track'},
+            'album': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['albums.Album']"}),
+            'artist': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['artists.Artist']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'order': ('django.db.models.fields.PositiveIntegerField', [], {}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
