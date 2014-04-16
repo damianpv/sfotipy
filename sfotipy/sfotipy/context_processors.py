ca6cb473c77fad01cfa939eedbab0863136b20a8
John Anderson <sontek@gmail.com>
2014-04-16 18:35:01 -0500

    CHG: Added changes.

--- sfotipy/sfotipy/context_processors.py
+++ sfotipy/sfotipy/context_processors.py
@@ -7,7 +7,9 @@ from tracks.models import Track
 def basico(request):
     tracks = Track.objects.all()
     track = None
+
     #import ipdb;ipdb.set_trace()
+
     for t in tracks:
 	if request.path == t.get_absolute_url():
 	    track = t
