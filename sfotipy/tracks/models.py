b8ce84d3115a4b126d008e60e30e34cbf9a6289d
John Anderson <sontek@gmail.com>
2014-04-16 18:35:01 -0500

    CHG: Added changes

--- sfotipy/tracks/models.py
+++ sfotipy/tracks/models.py
@@ -10,6 +10,9 @@ class Track(models.Model):
     album = models.ForeignKey(Album)
     artist = models.ForeignKey(Artist)
 
+    def get_absolute_url(self):
+	return '/tracks/%s/' % self.title
+
     def player(self):
         return """
         <audio controls>
